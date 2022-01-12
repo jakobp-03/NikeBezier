ed8d9d9edbbd87a802f0a0969dd4d3bc05b81300
Jakob Pavlovič <jakobp03@gmail.com>
2022-01-12 10:25:50 +0100

    Add files via upload

--- js/bgsshow.js
+++ js/bgsshow.js
@@ -1,16 +1,31 @@
-var body = document.getElementsByTagName("body")[0];
+var body = document.getElementById("bodi");
 var counter = 0;
 
 setInterval(function () {
-    if(counter ==0)
-  body.style.backgroundImage = "url()";
-    if(counter ==1)
-    body.style.backgroundImage = "url()";
-    if(counter ==2)
-  body.style.backgroundImage = "url()";
-  if(counter ==3){
-  body.style.backgroundImage = "url()";
+    if(counter ==0){
+      counter++;
+      body.style.backgroundImage = "url(image/bg1.jpg)";
+      console.log("Image1");
+    }
+  
+    else if(counter ==1){
+      body.style.backgroundImage = "url(image/bg2.jpg)";
+      counter++;
+      console.log("Image2");
+    }
+    
+    else if(counter ==2){
+      body.style.backgroundImage = "url(image/bg3.jpg)";
+      counter++;  
+      console.log("Image3");
+    }
+  
+  else if(counter ==3){
+  body.style.backgroundImage = "url(image/bg1.jpg)";
   counter =0;    
+  console.log("Image1");
   }
   
-}, 3000);
+}, 1500);
+
+
