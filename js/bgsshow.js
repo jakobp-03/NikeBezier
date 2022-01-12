6d52668b925cb84a71053900b6f0d74c6781ecd7
Jakob Pavlovič <jakobp03@gmail.com>
2022-01-12 18:25:44 +0100

    Add files via upload

--- js/bgsshow.js
+++ js/bgsshow.js
@@ -7,7 +7,7 @@ setInterval(function () {
       counter++;
       body.style.backgroundImage = "url(image/bg1.jpg)";
       html.style.background ="#526986";
-      document.getElementById("titl").style.color="white";
+      document.getElementById("titl").style.color="orangered";
       console.log("Image1");
     }
   
@@ -24,14 +24,22 @@ setInterval(function () {
       counter++;  
       html.style.background ="#759ea7";
       document.getElementById("titl").style.color="black";
+      document.getElementById("h51").style.color="#78a1e3";
+      document.getElementById("h52").style.color="#78a1e3";
+      document.getElementById("part").style.backgroundColor= "rgba(184, 227, 255, 0.6)";
+      document.getElementById("part2").style.backgroundColor= "rgba(184, 227, 255, 0.6)";
       console.log("Image3");
     }
   
   else if(counter ==3){
   body.style.backgroundImage = "url(image/bg1.jpg)";
   counter =0;    
-  document.getElementById("titl").style.color="white";
-  console.log("Image1");
+  document.getElementById("titl").style.color="orangered";
+  document.getElementById("h51").style.color="orangered";
+  document.getElementById("h52").style.color="orangered";
+  document.getElementById("part").style.backgroundColor= "rgba(240, 229, 228, 0.6)";
+  document.getElementById("part2").style.backgroundColor= "rgba(240, 229, 228, 0.6)";
+  console.log("Image4");
   }
   
 }, 5500);
